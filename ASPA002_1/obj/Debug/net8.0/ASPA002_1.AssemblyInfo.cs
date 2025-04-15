bdeca1fbba070172a00aca34e14ac39bb576c08e
Prohor Guletsky <iriwisiri@gmail.com>
2025-04-16 01:09:53 +0300

    5 лаба

--- ASPA002_1/obj/Debug/net8.0/ASPA002_1.AssemblyInfo.cs
+++ ASPA002_1/obj/Debug/net8.0/ASPA002_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASPA002_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8234befa31ff07ea3d7d3246fba5fd1e5745407")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+263f2a7c31cc255aab9ee45a378e90abf6288fc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASPA002_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASPA002_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DAL004/obj/Release/net9.0/DAL004.AssemblyInfo.cs
+++ DAL004/obj/Release/net9.0/DAL004.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL004")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8234befa31ff07ea3d7d3246fba5fd1e5745407")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+263f2a7c31cc255aab9ee45a378e90abf6288fc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL004")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL004")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
